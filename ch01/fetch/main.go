cd21ad23985f699d94dc338554770de93f93cc7b
daplho <nskirov@gmail.com>
2018-01-08 10:55:23 -0800

    Exercise 1.8: Modified fetch to add the 'http://' prefix

--- ch01/fetch/main.go
+++ ch01/fetch/main.go
@@ -5,11 +5,17 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
+		prefixedUrl := url
+		if !strings.HasPrefix(prefixedUrl, "http://") {
+			prefixedUrl = "http://" + url
+		}
+
+		resp, err := http.Get(prefixedUrl)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
