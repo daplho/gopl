29696788e407b589f27f51d9129faf674a2445ec
daplho <nskirov@gmail.com>
2018-02-22 10:14:02 -0800

    5.3

--- ch05/findlinks2/main.go
+++ ch05/findlinks2/main.go
@@ -3,20 +3,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"golang.org/x/net/html"
 )
 
 func main() {
-	doc, err := html.Parse(os.Stdin)
+	for _, url := range os.Args[1:] {
+		links, err := findLinks(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "findlinks2: %v\n", err)
+			continue
+		}
+		for _, link := range links {
+			fmt.Println(link)
+		}
+	}
+}
+
+// findLinks performs an HTTP GET request for each url, parses the
+// response as HTML, and extracts and returns the links.
+func findLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
-		os.Exit(1)
+		return nil, err
 	}
-	for _, link := range visit(nil, doc) {
-		fmt.Println(link)
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
 	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	return visit(nil, doc), nil
 }
 
 // visit appends to links each link found in n and returns the result.
@@ -29,11 +51,9 @@ func visit(links []string, n *html.Node) []string {
 		}
 	}
 
-	if n.FirstChild != nil {
-		links = visit(links, n.FirstChild)
-	}
-	if n.NextSibling != nil {
-		links = visit(links, n.NextSibling)
+	// iterative implementation
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
 	}
 
 	return links
