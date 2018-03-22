5528dbf407a78820d858ed7d73659d435df87893
daplho <nskirov@gmail.com>
2018-03-22 08:22:14 -0700

    6.5 intset 3

--- ch06/intset/main.go
+++ ch06/intset/main.go
@@ -6,7 +6,20 @@ import (
 )
 
 func main() {
-	fmt.Println("vim-go")
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String()) // "{1 9 144}"
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String()) // "{9 42}"
+
+	x.UnionWith(&y)
+	fmt.Println(x.String()) // "{1 9 42 144}"
+
+	fmt.Println(x.Has(9), x.Has(123)) // "true false"
 }
 
 // An IntSet is a set of small non-negative integers.
