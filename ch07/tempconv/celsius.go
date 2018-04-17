dc5f1aa1dc45edbc762a8b9ac73baa4f7820bcdf
daplho <nskirov@gmail.com>
2018-04-17 10:58:21 -0700

    7.4 added flag support for temperature in Kelvin

--- ch07/tempconv/celsius.go
+++ ch07/tempconv/celsius.go
@@ -7,10 +7,13 @@ import (
 )
 
 type Celsius float64
+type Kelvin float64
 type Fahrenheit float64
 
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func CToK(c Celsius) Kelvin     { return Kelvin(c + 273.15) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func KToC(k Kelvin) Celsius     { return Celsius(k - 273.15) }
 
 func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 
@@ -27,6 +30,9 @@ func (f *celsiusFlag) Set(s string) error {
 		return nil
 	case "F", "°F":
 		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K", "°K":
+		f.Celsius = KToC(Kelvin(value))
 		return nil
 	}
 	return fmt.Errorf("invalid temperature %q", s)
