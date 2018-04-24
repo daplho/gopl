e6f10fd9ab278ea22af35cd86324c08766324ac9
daplho <nskirov@gmail.com>
2018-04-24 12:48:26 -0700

    7.7 http5 added create and update

--- ch07/http5/main.go
+++ ch07/http5/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/create", db.create)
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
 	http.HandleFunc("/update", db.update)
@@ -20,6 +21,27 @@ type dollars float32
 func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
 type database map[string]dollars
+
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	priceString := req.URL.Query().Get("price")
+	_, ok := db[item]
+	if ok {
+		w.WriteHeader(http.StatusConflict)
+		fmt.Fprintf(w, "item '%s' already exists", item)
+		return
+	}
+
+	price, err := strconv.ParseFloat(priceString, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400
+		fmt.Fprintf(w, "bad value for new price: %q\n", priceString)
+		return
+	}
+
+	db[item] = dollars(price)
+	fmt.Fprintf(w, "%s\n", dollars(price))
+}
 
 func (db database) list(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
