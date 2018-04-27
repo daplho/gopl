49f1128af0c8e97615b9d1cd0a2f77d8eb2a3995
daplho <nskirov@gmail.com>
2018-04-27 14:44:17 -0700

    7.9 eval progress

--- ch07/eval/eval.go
+++ ch07/eval/eval.go
@@ -1,33 +1,17 @@
 package eval
 
-// An Expr is an arithmetic expression
-type Expr interface {
-	// Eval returns the value of this Expr in the environment env.
-	Eval(env Env) float64
-	// Check reports errors in this Expr and adds its Vars to the set.
-	Check(vars map[Var]bool) error
+type Env map[Var]float64
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
 }
 
-// A Var identifies a variable, e.g., x.
-type Var string
-
-// A literal is a numeric constant, e.g., 3.141.
-type literal float64
-
-// A unary represents a unary operator expression, e.g., -x.
-type unary struct {
-	op rune // one of '+', '-'
-	x  Expr
+func (v literal) Eval(_ Env) float64 {
+	return float64(l)
 }
 
-// A binary represents a binary operator expression, e.g., x+y.
-type binary struct {
-	op   rune // one of '+', '-', '*', '/'
-	x, y Expr
-}
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
 
-// A call represents a function call expression, e.g., sin(x).
-type call struct {
-	fn   string // one of "pow", "sin", "sqrt"
-	args []Expr
+	}
 }
