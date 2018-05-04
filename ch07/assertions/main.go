c5ce758a773c1639cddd34c72eac7221deebed87
daplho <nskirov@gmail.com>
2018-05-04 13:16:53 -0700

    adding a bit of meat to assertion example

--- ch07/assertions/main.go
+++ ch07/assertions/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "bytes"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -14,10 +14,23 @@ func main() {
 
 	// possibility 1: asserted type `T` is a concrete type
 	// (`*os.File` and `*bytes.Buffer` respectively below)
-	f := x.(*os.File) // success: f == os.Stdout
-	fmt.Printf("os.Stdout's name is %s\n", f.Name())
-	// c := x.(*bytes.Buffer) // panic: interface holds *os.File, not *bytes.Buffer
-	// fmt.Printf("os.Stdout's contents are %s\n", c.String())
+	if f, ok := x.(*os.File); ok { // success: f == os.Stdout
+		fmt.Println("Successfully asserted 'x' to an '*os.File'")
+		fmt.Printf("os.Stdout's name is %s\n", f.Name())
+	}
+
+	if c, ok := x.(*bytes.Buffer); !ok {
+		fmt.Println("panic: interface holds *os.File, not *bytes.Buffer (this is expected)")
+	} else {
+		fmt.Printf("os.Stdout asserted to a '*bytes.Buffer' contains: %s", c.String())
+	}
 
 	// possibility 2: asserted type `T` is an interface type
+	//	var y io.Writer
+	//	y = os.Stdout
+	//	rw := y.(io.ReadWriter) // success: *os.File has both Read and Write
+	//	fmt.Printf("os.Stdout's type is %T\n", rw)
+	//
+	//	y = new(ByteCounter)
+	//	rw = y.(io.ReadWriter) // panic: *ByteCounter has no Read method
 }
