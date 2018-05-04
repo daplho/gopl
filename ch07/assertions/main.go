13be073728ea21c50584130015f4f24747e47d95
daplho <nskirov@gmail.com>
2018-05-04 13:37:23 -0700

    cleaning up assertion example

--- ch07/assertions/main.go
+++ ch07/assertions/main.go
@@ -7,6 +7,13 @@ import (
 	"os"
 )
 
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
 func main() {
 	var x io.Writer
 	x = os.Stdout
@@ -14,8 +21,8 @@ func main() {
 
 	// possibility 1: asserted type `T` is a concrete type
 	// (`*os.File` and `*bytes.Buffer` respectively below)
-	if f, ok := x.(*os.File); ok { // success: f == os.Stdout
-		fmt.Println("Successfully asserted 'x' to an '*os.File'")
+	if f, ok := x.(*os.File); ok {
+		fmt.Println("success: f == os.Stdout")
 		fmt.Printf("os.Stdout's name is %s\n", f.Name())
 	}
 
@@ -26,11 +33,18 @@ func main() {
 	}
 
 	// possibility 2: asserted type `T` is an interface type
-	//	var y io.Writer
-	//	y = os.Stdout
-	//	rw := y.(io.ReadWriter) // success: *os.File has both Read and Write
-	//	fmt.Printf("os.Stdout's type is %T\n", rw)
-	//
-	//	y = new(ByteCounter)
-	//	rw = y.(io.ReadWriter) // panic: *ByteCounter has no Read method
+	var y io.Writer
+	y = os.Stdout
+
+	if rw, ok := y.(io.ReadWriter); ok {
+		fmt.Println("success: *os.File has both Read and Write (this is expected)")
+		rw.Write([]byte("Hello Assertions!\n"))
+	}
+
+	y = new(ByteCounter)
+	if rw, ok := y.(io.ReadWriter); !ok {
+		fmt.Println("panic: *ByteCounter has no Read method (this is expected)")
+	} else {
+		rw.Write([]byte("Goodbye Assertions!\n"))
+	}
 }
