2dce206cf95e3c7d300a89d15dc8ad423f0b3efc
daplho <nskirov@gmail.com>
2018-05-29 14:52:43 -0700

    ch08 thumbnails6

--- ch08/thumbnail/thumbnail_test.go
+++ ch08/thumbnail/thumbnail_test.go
@@ -5,6 +5,8 @@ package thumbnail_test
 
 import (
 	"log"
+	"os"
+	"sync"
 
 	"github.com/daplho/gopl/ch08/thumbnail"
 )
@@ -89,4 +91,37 @@ func makeThumbnails5(filenames []string) (thumbfiles []string, err error) {
 	}
 
 	return thumbfiles, nil
+}
+
+// makeThumbnails6 makes thumbnails for each file received from the channel.
+// It returns the number of bytes occupied by the files it creates.
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup // number of working goroutines
+	for f := range filenames {
+		wg.Add(1)
+		// worker
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb) // OK to ignore error
+			sizes <- info.Size()
+		}(f)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+	return total
 }
