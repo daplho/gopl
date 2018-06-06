8138fa084d0276d3fba606c6ca9fb9340c4ffd1b
daplho <nskirov@gmail.com>
2018-06-05 18:06:09 -0700

    ch08 crawl1

---
+++ ch08/crawl1/findlinks.go
@@ -0,0 +1,44 @@
+// Crawl1 crawls web links starting with command-line arguments.
+//
+// This version quickly exhausts available file descriptors
+// due to excessive concurrent calls to links.Extract.
+//
+// Also, it never terminates because the worklist is never closed.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/daplho/gopl/ch05/links"
+)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+
+	// Start with the command-line arguments.
+	go func() { worklist <- os.Args[1:] }()
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, link := range list {
+			if !seen[link] {
+				seen[link] = true
+				go func(link string) {
+					worklist <- crawl(link)
+				}(link)
+			}
+		}
+	}
+}
