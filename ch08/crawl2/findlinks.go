25685ca6937e849c3264a9fc40041a5710153869
daplho <nskirov@gmail.com>
2018-06-06 12:19:31 -0700

    ch08 crawl2

---
+++ ch08/crawl2/findlinks.go
@@ -0,0 +1,55 @@
+// Crawl1 crawls web links starting with command-line arguments.
+//
+// This version quickly exhausts available file descriptors
+// due to excessive concurrent calls to links.Extract.
+//
+// Also, it never terminates because the worklist is never closed.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/daplho/gopl/ch05/links"
+)
+
+// tokens is a counting semaphore used to
+// enforce a limit of 20 concurrent requests.
+var tokens = make(chan struct{}, 20)
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	tokens <- struct{}{} // acquire a token
+	list, err := links.Extract(url)
+	<-tokens // release the token
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func main() {
+	worklist := make(chan []string)
+	var n int // number of pending sends to worklist
+
+	// Start with the command-line arguments.
+	n++
+	go func() { worklist <- os.Args[1:] }()
+
+	// Crawl the web concurrently.
+	seen := make(map[string]bool)
+	for ; n > 0; n-- {
+		for list := range worklist {
+			for _, link := range list {
+				if !seen[link] {
+					seen[link] = true
+					n++
+					go func(link string) {
+						worklist <- crawl(link)
+					}(link)
+				}
+			}
+		}
+	}
+}
