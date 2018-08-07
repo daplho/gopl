61cfe1a112e784fac2f31032262ba04ef6b1a115
daplho <nskirov@gmail.com>
2018-08-07 11:50:43 -0700

    ch09 memo4

---
+++ ch09/memo4/memo_test.go
@@ -0,0 +1,21 @@
+package memo_test
+
+import (
+	"testing"
+
+	"github.com/daplho/gopl/ch09/memo3"
+	"github.com/daplho/gopl/ch09/memotest"
+)
+
+var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Sequential(t, m)
+}
+
+// NOTE: not concurrency-safe! Test fails.
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
