837b2180f320e07a087d943f9c90191449654321
daplho <nskirov@gmail.com>
2019-01-04 10:25:35 -0800

    added cases to test

--- ch12/format/format_test.go
+++ ch12/format/format_test.go
@@ -13,4 +13,6 @@ func Test(t *testing.T) {
 	var d time.Duration = 1 * time.Nanosecond
 	fmt.Println(format.Any(x))
 	fmt.Println(format.Any(d))
+	fmt.Println(format.Any([]int64{x}))
+	fmt.Println(format.Any([]time.Duration{d}))
 }
